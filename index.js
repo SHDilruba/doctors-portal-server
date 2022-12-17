1ef082de97c6d4db8e318971d9470bae06e5632c
unknown <hasinadilruba2021@gmail.com>
2022-12-17 23:25:20 +0100

    get bookings data by email done

--- index.js
+++ index.js
@@ -15,11 +15,51 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
      try{
           const appointmentOptionCollection = client.db('doctors-portal').collection('appointmentOption');
-          app.get ('/appointmentOption', async(req, res) =>{
+           const bookingsCollection = client.db('doctors-portal').collection('bookings');
+
+          app.get ('/appointmentOptions', async(req, res) =>{
+            const date = req.query.date;
               const query = {};
               const options = await appointmentOptionCollection.find(query).toArray();
+
+              const bookingQuery = {appointmentDate: date}
+              const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+              options.forEach(option => {
+                   const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                   const bookedSlots = optionBooked.map(book => book.slot)
+                   const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                   option.slots =  remainingSlots;
+              })
               res.send(options);
+          });
+    
+          app.get('/bookings', async(req, res) =>{
+                const email = req.query.email;
+                console.log(email);
+                const query = {email: email};
+                const bookings = await bookingsCollection.find(query).toArray();
+                res.send(bookings);
           })
+
+          app.post('/bookings', async(req, res) =>{
+              const booking = req.body
+              console.log(booking);
+               const query = {
+                   appointmentDate: booking.appointmentDate,
+                   email: booking.email,
+                   treatment: booking.treatment
+               }
+
+               const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+               if(alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})  
+               }
+
+              const result = await bookingsCollection.insertOne(booking);
+              res.send(result);
+          });
      }
      finally{
          
